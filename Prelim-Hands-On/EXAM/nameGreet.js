93d70e1736da62cae2d4597d679ccb8bcf1184eb
RolynMeow <122800669+RolynMeow@users.noreply.github.com>
2023-02-08 15:39:03 +0800

    Initial commit

---
+++ Prelim-Hands-On/EXAM/nameGreet.js
@@ -0,0 +1,4 @@
+var greetings = require('./greet');
+
+console.log(greetings("Rolyn"));
+console.log(greetings("Fernan"));
---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-tlvwbn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-tlvwbn)
